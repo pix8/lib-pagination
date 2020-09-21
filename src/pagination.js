991390cc19c9505f7fb64697cd69e475d07629fa
Jonathan Brincat <jonathan@pix8.co.uk>
2020-09-21 22:18:57 +0100

    example page whipped up

--- src/pagination.js
+++ src/pagination.js
@@ -1,16 +1,15 @@
-const 	NAME 			= "pagination",
+const NAME 			= "pagination",
 		AUTHOR			= "j.brincat",
 		VERSION			= "0.0.1";
 
 /**
- * Generates an array to be used for rendering of pagination
+ * Generates an array to be used for rendering of pagination - suitable for use with adonisjs
  * @param {number} current - The current page
  * @param {number} total - Last page of the collection
  * @param {number} delta - Spread of indices to represent in the collection
  * @returns {array} Array of page indices within the scoped range complimented by first and last page and ellipsises used as separators
  */
 export default function Pagination(current, total, delta = 5) {
-
   const noop = (v, indice) => (total === 1 || indice%total === 1) ? [] : (v === 1) ? [v, '...'] : ['...', v];
 
   if(delta > total) delta = total;
