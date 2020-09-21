2bffd751adf7a98ad0d956cbd79b612d6846546e
Jonathan Brincat <jonathan@pix8.co.uk>
2020-09-21 01:50:12 +0100

    gh-pages inital commit - based on ui-accordion gh-pages branch

--- example/src/index.js
+++
@@ -1,9 +0,0 @@
-import Paginate from "@pix8/lib-pagination";
-
-const curr = 1;
-const total = 10;
-const delta = 5;
-
-for(let i = 1; i <= total; i++) {
-  console.log('page = ', i, ' :: ', Paginate(i, total, delta));
-}
