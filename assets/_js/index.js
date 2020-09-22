ec62b98a53ae1ae9c3c64086837218845ca70833
Jonathan Brincat <jonathan@pix8.co.uk>
2020-09-22 14:50:27 +0100

    removed redundant files and configurations in jekyll; prep for change of module

--- assets/_js/index.js
+++ assets/_js/index.js
@@ -1,10 +1,11 @@
 import Accordion from "@pix8/ui-accordion";
+import Pagination from "@pix8/lib-pagination";
 
 
-document.documentElement.classList.remove("client__no-js");
+// document.documentElement.classList.remove("client__no-js");
 
-var $$accordion = document.querySelectorAll(".ui__accordion");
+// var $$accordion = document.querySelectorAll(".ui__accordion");
 
-[].forEach.call($$accordion, ($accordion, i) => {
-	new Accordion($accordion);
-});
+// [].forEach.call($$accordion, ($accordion, i) => {
+// 	new Accordion($accordion);
+// });
