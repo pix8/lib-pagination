7c9ee5f0a550e46b4c30a6083cc555c144ca2363
Jonathan Brincat <jonathan@pix8.co.uk>
2020-09-22 15:39:46 +0100

    migration of example from project directory to github pages jekyll site

--- assets/_js/index.js
+++ assets/_js/index.js
@@ -1,11 +1,67 @@
-import Accordion from "@pix8/ui-accordion";
-import Pagination from "@pix8/lib-pagination";
+import Paginate from '@pix8/lib-pagination';
 
+const current = 1, total = 10, delta = 5;
 
-// document.documentElement.classList.remove("client__no-js");
+const $example = document.getElementById('example');
 
-// var $$accordion = document.querySelectorAll(".ui__accordion");
+for(let i = current; i <= total; i++) {
+  const node = {
+    wrapper: document.createElement('li'),
+    paragraph: document.createElement('p'),
+    pre: document.createElement('pre'),
+    code: document.createElement('code'),
+    pagination: document.createElement('ul'),
+    nav: document.createElement('nav')
+  };
 
-// [].forEach.call($$accordion, ($accordion, i) => {
-// 	new Accordion($accordion);
-// });
+  const text = {
+    label: document.createTextNode(`Current page = ${i}`),
+    index: document.createTextNode(`${JSON.stringify(Paginate(i, total, delta))}`),
+  }
+
+  node.paragraph.classList.add('example__label');
+  node.pre.classList.add('example__code');
+  node.nav.classList.add('example__pagination');
+
+  node.paragraph.appendChild(text.label);
+  node.wrapper.appendChild(node.paragraph);
+
+  node.code.appendChild(text.index);
+  node.pre.appendChild(node.code);
+  node.wrapper.appendChild(node.pre);
+
+  for(const page of Paginate(i, total, delta)) {
+    const pNode = {
+      listItem: document.createElement('li'),
+      link: document.createElement('a'),
+      span: document.createElement('span')
+    }
+
+    const pText = {
+      page: document.createTextNode(page)
+    }
+
+    pNode.link.classList.add('btn');
+
+    if(page === i) {
+      pNode.listItem.classList.add('active');
+    }else {
+      pNode.link.href = `?page=${page}`
+    }
+
+    if(page === '...') {
+      pNode.span.appendChild(pText.page);
+      pNode.listItem.appendChild(pNode.span);
+    }else {
+      pNode.link.appendChild(pText.page);
+      pNode.listItem.appendChild(pNode.link);
+    }
+
+    node.pagination.appendChild(pNode.listItem);
+  }
+
+  node.nav.appendChild(node.pagination);
+  node.wrapper.appendChild(node.nav);
+
+  $example.appendChild(node.wrapper);
+}
--- assets/scripts/roll.app.bundle.js
+++ assets/scripts/roll.app.bundle.js
@@ -1,6 +1,138 @@
 (function () {
   'use strict';
 
-  !function(t,e){void 0===e&&(e={});var n=e.insertAt;if(t&&"undefined"!=typeof document){var i=document.head||document.getElementsByTagName("head")[0],o=document.createElement("style");o.type="text/css","top"===n&&i.firstChild?i.insertBefore(o,i.firstChild):i.appendChild(o),o.styleSheet?o.styleSheet.cssText=t:o.appendChild(document.createTextNode(t));}}(".ui__accordion .ui__tab {\n  position: relative; }\n  .ui__accordion .ui__tab .ui__toggle {\n    display: block;\n    cursor: pointer; }\n\n.ui__accordion .ui__pane {\n  position: relative;\n  overflow: hidden; }\n\n.ui__accordion .ui__tab:not([aria-expanded=true]):not([aria-selected=true]):not(.state__active):not(.state__transition) + .ui__pane,\n.ui__accordion .ui__tab:not([aria-expanded=true]):not([aria-selected=true]):not(.state__active):not(.state__transition) + [aria-hidden=true] {\n  height: 0;\n  display: none; }\n");
+  function _toConsumableArray(arr) {
+    return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread();
+  }
+
+  function _arrayWithoutHoles(arr) {
+    if (Array.isArray(arr)) return _arrayLikeToArray(arr);
+  }
+
+  function _iterableToArray(iter) {
+    if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter);
+  }
+
+  function _unsupportedIterableToArray(o, minLen) {
+    if (!o) return;
+    if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+    var n = Object.prototype.toString.call(o).slice(8, -1);
+    if (n === "Object" && o.constructor) n = o.constructor.name;
+    if (n === "Map" || n === "Set") return Array.from(o);
+    if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+  }
+
+  function _arrayLikeToArray(arr, len) {
+    if (len == null || len > arr.length) len = arr.length;
+
+    for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+
+    return arr2;
+  }
+
+  function _nonIterableSpread() {
+    throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+  }
+
+  /**
+   * Generates an array to be used for rendering of pagination - suitable for use with adonisjs
+   * @param {number} current - The current page
+   * @param {number} total - Last page of the collection
+   * @param {number} delta - Spread of indices to represent in the collection
+   * @returns {array} Array of page indices within the scoped range complimented by first and last page and ellipsises used as separators
+   */
+
+  function Pagination(current, total) {
+    var delta = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 5;
+
+    var noop = function noop(v, indice) {
+      return total === 1 || indice % total === 1 ? [] : v === 1 ? [v, '...'] : ['...', v];
+    };
+
+    if (delta > total) delta = total;
+    var indice = current - Math.floor(delta / 2);
+    indice = Math.max(indice, 1);
+    indice = Math.min(indice, 1 + (total - delta));
+    return [].concat(_toConsumableArray(noop(1, indice)), _toConsumableArray(Array.from({
+      length: delta
+    }, function () {
+      return indice++;
+    })), _toConsumableArray(noop(total, indice)));
+  }
+
+  function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray$1(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+  function _unsupportedIterableToArray$1(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray$1(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray$1(o, minLen); }
+
+  function _arrayLikeToArray$1(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+  var current = 1,
+      total = 10,
+      delta = 5;
+  var $example = document.getElementById('example');
+
+  for (var i = current; i <= total; i++) {
+    var node = {
+      wrapper: document.createElement('li'),
+      paragraph: document.createElement('p'),
+      pre: document.createElement('pre'),
+      code: document.createElement('code'),
+      pagination: document.createElement('ul'),
+      nav: document.createElement('nav')
+    };
+    var text = {
+      label: document.createTextNode("Current page = ".concat(i)),
+      index: document.createTextNode("".concat(JSON.stringify(Pagination(i, total, delta))))
+    };
+    node.paragraph.classList.add('example__label');
+    node.pre.classList.add('example__code');
+    node.nav.classList.add('example__pagination');
+    node.paragraph.appendChild(text.label);
+    node.wrapper.appendChild(node.paragraph);
+    node.code.appendChild(text.index);
+    node.pre.appendChild(node.code);
+    node.wrapper.appendChild(node.pre);
+
+    var _iterator = _createForOfIteratorHelper(Pagination(i, total, delta)),
+        _step;
+
+    try {
+      for (_iterator.s(); !(_step = _iterator.n()).done;) {
+        var page = _step.value;
+        var pNode = {
+          listItem: document.createElement('li'),
+          link: document.createElement('a'),
+          span: document.createElement('span')
+        };
+        var pText = {
+          page: document.createTextNode(page)
+        };
+        pNode.link.classList.add('btn');
+
+        if (page === i) {
+          pNode.listItem.classList.add('active');
+        } else {
+          pNode.link.href = "?page=".concat(page);
+        }
+
+        if (page === '...') {
+          pNode.span.appendChild(pText.page);
+          pNode.listItem.appendChild(pNode.span);
+        } else {
+          pNode.link.appendChild(pText.page);
+          pNode.listItem.appendChild(pNode.link);
+        }
+
+        node.pagination.appendChild(pNode.listItem);
+      }
+    } catch (err) {
+      _iterator.e(err);
+    } finally {
+      _iterator.f();
+    }
+
+    node.nav.appendChild(node.pagination);
+    node.wrapper.appendChild(node.nav);
+    $example.appendChild(node.wrapper);
+  }
 
 }());
