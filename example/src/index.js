0c7662a6811624b981bae066514c843a07391b05
Jonathan Brincat <jonathan@pix8.co.uk>
2020-09-22 14:09:02 +0100

    tidy and build for gh-pages export

--- example/src/index.js
+++ example/src/index.js
@@ -1,59 +1,68 @@
 import Paginate from '@pix8/lib-pagination';
 import { $ } from './util';
 
-const curr = 1;
-const total = 10;
-const delta = 5;
+const current = 1, total = 10, delta = 5;
 
 const $example = $('#example');
 
-for(let i = 1; i <= total; i++) {
-  let wrapperNode = document.createElement('li');
-  let paragraphNode = document.createElement('p');
-  let preNode = document.createElement('pre');
-  let codeNode = document.createElement('code');
-  let pagination = document.createElement('ul');
-  let navNode = document.createElement('nav');
-  let labelNode = document.createTextNode(`Current page = ${i}`);
-  let textNode = document.createTextNode(`${JSON.stringify(Paginate(i, total, delta))}`);
+for(let i = current; i <= total; i++) {
+  const node = {
+    wrapper: document.createElement('li'),
+    paragraph: document.createElement('p'),
+    pre: document.createElement('pre'),
+    code: document.createElement('code'),
+    pagination: document.createElement('ul'),
+    nav: document.createElement('nav')
+  };
 
-  paragraphNode.classList.add('example__label');
-  preNode.classList.add('example__code');
-  navNode.classList.add('example__pagination');
-
-  paragraphNode.appendChild(labelNode);
-  wrapperNode.appendChild(paragraphNode);
-
-  codeNode.appendChild(textNode);
-  preNode.appendChild(codeNode);
-  wrapperNode.appendChild(preNode);
-
-  for(const page of Paginate(i, total, delta)) {
-    let listItemNode = document.createElement('li');
-    let linkNode = document.createElement('a');
-    let spanNode = document.createElement('span');
-    let textNode = document.createTextNode(page);
-
-    if(page === i) {
-      listItemNode.classList.add('active');
-    }else {
-      linkNode.href = `?page=${page}`
-    }
-    linkNode.classList.add('btn');
-
-    if(page === '...') {
-      spanNode.appendChild(textNode);
-      listItemNode.appendChild(spanNode);
-    }else {
-      linkNode.appendChild(textNode);
-      listItemNode.appendChild(linkNode);
-    }
-
-    pagination.appendChild(listItemNode);
+  const text = {
+    label: document.createTextNode(`Current page = ${i}`),
+    index: document.createTextNode(`${JSON.stringify(Paginate(i, total, delta))}`),
   }
 
-  navNode.appendChild(pagination);
-  wrapperNode.appendChild(navNode);
+  node.paragraph.classList.add('example__label');
+  node.pre.classList.add('example__code');
+  node.nav.classList.add('example__pagination');
 
-  $example.appendChild(wrapperNode);
+  node.paragraph.appendChild(text.label);
+  node.wrapper.appendChild(node.paragraph);
+
+  node.code.appendChild(text.index);
+  node.pre.appendChild(node.code);
+  node.wrapper.appendChild(node.pre);
+
+  for(const page of Paginate(i, total, delta)) {
+    const pNode = {
+      listItem: document.createElement('li'),
+      link: document.createElement('a'),
+      span: document.createElement('span')
+    }
+
+    const pText = {
+      page: document.createTextNode(page)
+    }
+
+    pNode.link.classList.add('btn');
+
+    if(page === i) {
+      pNode.listItem.classList.add('active');
+    }else {
+      pNode.link.href = `?page=${page}`
+    }
+
+    if(page === '...') {
+      pNode.span.appendChild(pText.page);
+      pNode.listItem.appendChild(pNode.span);
+    }else {
+      pNode.link.appendChild(pText.page);
+      pNode.listItem.appendChild(pNode.link);
+    }
+
+    node.pagination.appendChild(pNode.listItem);
+  }
+
+  node.nav.appendChild(node.pagination);
+  node.wrapper.appendChild(node.nav);
+
+  $example.appendChild(node.wrapper);
 }
